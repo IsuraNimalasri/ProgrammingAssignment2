c076511159ae0f75ed33c1ac8aa5c44c2654fcff
Isura Nimalasiri <isuranimalasiri@gmail.com>
2017-04-27 12:19:17 +0530

    3rd week Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
+  inve <- NULL
+  set <- function(y) {
+    x <<- y
+    inve <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inve <<- inverse
+  getinverse <- function() inve
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+     if(!is.null(inv)) {
+           message("getting cached data is ready")
+          return(inv)
+      }
+     data <- x$get()
+      inv <- solve(data, ...)
+      x$setinverse(inv)
+      inv
+  
         ## Return a matrix that is the inverse of 'x'
 }
