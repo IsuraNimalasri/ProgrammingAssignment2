234b2933954c7a52bf8d49576f0a70ef265b6e65
Isura Nimalasiri <isuranimalasiri@gmail.com>
2017-04-29 17:19:29 +0530

    mysecond_try

--- cachematrix.R
+++ cachematrix.R
@@ -3,15 +3,24 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-  inve <- NULL
-  set <- function(y) {
-    x <<- y
+makeCacheMatrix <- function(x = matrix()) { # ragument is matrix
+  inve <- NULL #make inve variable in cache
+  # store a matrix
+  set <- function(y) { #
+    x <<- y 
+    # since the matrix is assigned  y, flush the cache
     inve <<- NULL
   }
-  get <- function() x
-  setinverse <- function(inverse) inve <<- inverse
-  getinverse <- function() inve
+  # returns the stored matrix
+  get <- function() {x}
+  
+  # cache the given argument 
+  setinverse <- function(inverse){ inve <<- inverse}
+  
+  # get the cached value
+  getinverse <- function() {inve}
+  
+  # return a list. Each named element of the list is a function
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -21,11 +30,18 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  
+  # get the cached value
   inv <- x$getinverse()
+  
+  # if a cached value exists return it
      if(!is.null(inv)) {
            message("getting cached data is ready")
           return(inv)
-      }
+     }
+  
+  # otherwise get the matrix, caclulate the inverse and store it in
+  # the cache
      data <- x$get()
       inv <- solve(data, ...)
       x$setinverse(inv)
